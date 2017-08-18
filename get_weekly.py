d23370bac0deb47014feb49b45544e2ceeec6407
Andy <amckay@mozilla.com>
2017-08-18 15:16:54 -0700

    simplify

--- get_weekly.py
+++ get_weekly.py
@@ -8,9 +8,11 @@ import requests
 CLUBS = [197162, 2298]
 ROOT_URL = 'https://www.strava.com/clubs/'
 
+offset = 0
+
 today = datetime.today()
 today_str = today.strftime('%Y-%m-%d')
-start_week = today - timedelta(days=today.weekday())
+start_week = today - timedelta(days=today.weekday()) - timedelta(days=offset*7)
 start_week_str = start_week.strftime('%Y-%m-%d')
 
 
@@ -19,58 +21,38 @@ def get(path):
         'Accept': 'text/javascript, application/javascript'
     }
     url = '%s%d/leaderboard/' % (ROOT_URL, path)
+    if offset:
+        url += '?week_offset=' + str(offset)
     res = requests.get(url, headers=headers)
     return res.json()
 
 
-def get_clubs():
+def summarize():
     for club in CLUBS:
+        filename = 'summaries/summary.{}.json'.format(club)
         try:
-            data = json.load(open('data/data.{}.json'.format(club), 'r'))
-        except ValueError:
+            data = json.load(open(filename, 'r'))
+        except (ValueError, IOError):
             data = {}
+
+        id = start_week_str
+        athlete_set = set()
+        data[id] = {'distance': 0, 'athletes': 0}
         leaderboard = get(club)
         for entry in leaderboard['data']:
-            id = '%s-%s' % (start_week_str, entry['best_activities_distance_activity_id'])
             if int(entry['distance']) == 0:
                 continue
-            data[id] = {
-                'week': start_week_str,
-                'date': today_str,
-                'athlete': entry['athlete_id'],
-                'distance': entry['distance'],
-            }
+            athlete_set.add(entry['athlete_id'])
+            data[id]['distance'] = data[id]['distance'] + entry['distance']
+            data[id]['athletes'] = len(athlete_set)
 
         sorted_data = OrderedDict()
-        keys = sorted(data.keys())
+        keys = reversed(sorted(data.keys()))
         for key in keys:
             sorted_data[key] = data[key]
 
-        json.dump(sorted_data, open('data/data.{}.json'.format(club), 'w'), indent=2)
-
-
-def summarize():
-    for filename in os.listdir('data'):
-        if filename.endswith('.json'):
-            file_id = filename.split('.')[1]
-            data = {}
-            try:
-                entries = json.load(open('data/{}'.format(filename)))
-            except ValueError:
-                entries = {}
-            for key, entry in entries.items():
-                data.setdefault(entry['week'], {'distance': 0, 'athletes': 0})
-                data[entry['week']]['distance'] += entry['distance']
-                data[entry['week']]['athletes'] += 1
-
-            sorted_data = OrderedDict()
-            keys = reversed(sorted(data.keys()))
-            for key in keys:
-                sorted_data[key] = data[key]
-
-            json.dump(sorted_data, open('summaries/summary.{}.json'.format(file_id), 'w'), indent=2)
+        json.dump(sorted_data, open(filename, 'w'), indent=2)
 
 
 if __name__ == '__main__':
-    get_clubs()
     summarize()
